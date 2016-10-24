2e8a6697b5f78599aa7599e6d161a6730256b292
Tyler Bird <tyler_b_bird@yahoo.com>
2016-10-24 11:10:17 -0500

    feat: add buildContours function which finds contours

--- main.cpp
+++ main.cpp
@@ -18,7 +18,14 @@ map<string,int> BGR = {
     {"rl", 145},
     {"rh", 255}};
 
+map<string,int> thresh = {
+    {"Thresh", 0}
+};
+
+RNG rng(12345);
+
 Mat findObject(Mat &orig);
+Mat buildContours(Mat &orig);
 
 int main(/*int argc, char *argv[]*/)
 {
@@ -41,6 +48,7 @@ int main(/*int argc, char *argv[]*/)
     // Create windows and trackbars
     namedWindow(rgbwindow,CV_WINDOW_AUTOSIZE);
     namedWindow("depth",CV_WINDOW_AUTOSIZE);
+    namedWindow("Contours", CV_WINDOW_AUTOSIZE);
 
     createTrackbar("rh", rgbwindow, &BGR.at("rh"), 255);
     createTrackbar("rl", rgbwindow, &BGR.at("rl"), 255);
@@ -48,6 +56,8 @@ int main(/*int argc, char *argv[]*/)
     createTrackbar("gl", rgbwindow, &BGR.at("gl"), 255);
     createTrackbar("bh", rgbwindow, &BGR.at("bh"), 255);
     createTrackbar("bl", rgbwindow, &BGR.at("bl"), 255);
+
+    createTrackbar("Threshold:", "Contours", &thresh.at("Thresh"), 255);
 
     device.startVideo();
     device.startDepth();
@@ -57,6 +67,7 @@ int main(/*int argc, char *argv[]*/)
         device.getVideo(rgbMat);
         device.getDepth(depthMat);
         imshow(rgbwindow, findObject(rgbMat));
+        imshow("Contours", buildContours(rgbMat));
         depthMat.convertTo(depthf, CV_8UC1, 255.0/2048.0);
         imshow("depth",depthf);
 
@@ -116,4 +127,47 @@ Mat findObject(Mat &orig){
 
     // Return tracklines
     return orig;
+}
+
+// buildContours takes a matrix, finds contours, then adds
+// bounding boxes to help track them
+Mat buildContours(Mat &orig){
+    // Define variables
+    Mat orig_gray;
+    Mat threshold_output;
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+
+    /// Convert image to gray and blur it
+    cvtColor(orig, orig_gray, CV_BGR2GRAY);
+    blur(orig_gray, orig_gray, Size(3,3));
+
+    /// Detect edges using Threshold
+    threshold(orig_gray, threshold_output, thresh.at("Thresh"), 255, THRESH_BINARY);
+    /// Find contours
+    findContours(threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+    /// Approximate contours to polygons + get bounding rects and circles
+    vector<vector<Point> > contours_poly( contours.size() );
+    vector<Rect> boundRect( contours.size() );
+    vector<Point2f>center( contours.size() );
+    vector<float>radius( contours.size() );
+
+    for(unsigned int i = 0; i < contours.size(); i++){
+        approxPolyDP( Mat(contours[i]), contours_poly[i], 3, false);
+        boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+        minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
+    }
+
+    /// Draw polygonal contour + bonding rects + circles
+    Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
+    for(unsigned int i = 0; i< contours.size(); i++){
+        Scalar color = Scalar(rng.uniform(0,255), rng.uniform(0,255), rng.uniform(0,255) );
+        if(radius[i] > 7){
+            drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+            rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+            circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
+        }
+    }
+    return drawing;
 }
