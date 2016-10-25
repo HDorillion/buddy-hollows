5076cb59fa358a92fc2389c7e520a27fd4be08a1
Tyler Bird <tyler_b_bird@yahoo.com>
2016-10-24 21:23:03 -0500

    fix: modify differentiateObjects to create clusters of objects correctly

--- main.cpp
+++ main.cpp
@@ -11,21 +11,22 @@ using namespace cv;
 
 // Global variables
 map<string,int> HSV = {
-    {"Hl", 0},
-    {"Hh", 10},
-    {"Sl", 175},
+    {"Hl", 10},
+    {"Hh", 20},
+    {"Sl", 125},
     {"Sh", 255},
     {"Vl", 100},
     {"Vh", 255}};
 
 map<string,int> thresh = {
-    {"Thresh", 0}
+    {"Thresh", 110}
 };
 
 RNG rng(12345);
 
 Mat findObject(Mat &orig);
 Mat buildContours(Mat &orig);
+Mat differentiateObjects(Mat &orig, Scalar HSVlb, Scalar HSVub);
 
 int main(/*int argc, char *argv[]*/)
 {
@@ -49,16 +50,16 @@ int main(/*int argc, char *argv[]*/)
     namedWindow(hsvtrackbars, CV_WINDOW_AUTOSIZE);
     namedWindow(rgbwindow,CV_WINDOW_AUTOSIZE);
     namedWindow("depth",CV_WINDOW_AUTOSIZE);
-    namedWindow("Contours", CV_WINDOW_AUTOSIZE);
+//    namedWindow("Contours", CV_WINDOW_AUTOSIZE);
 
-//    createTrackbar("Hh", hsvtrackbars, &HSV.at("Hh"), 179);
-//    createTrackbar("Hl", hsvtrackbars, &HSV.at("Hl"), 179);
-//    createTrackbar("Sh", hsvtrackbars, &HSV.at("Sh"), 255);
-//    createTrackbar("Sl", hsvtrackbars, &HSV.at("Sl"), 255);
-//    createTrackbar("Vh", hsvtrackbars, &HSV.at("Vh"), 255);
-//    createTrackbar("Vl", hsvtrackbars, &HSV.at("Vl"), 255);
+    createTrackbar("Hh", hsvtrackbars, &HSV.at("Hh"), 179);
+    createTrackbar("Hl", hsvtrackbars, &HSV.at("Hl"), 179);
+    createTrackbar("Sh", hsvtrackbars, &HSV.at("Sh"), 255);
+    createTrackbar("Sl", hsvtrackbars, &HSV.at("Sl"), 255);
+    createTrackbar("Vh", hsvtrackbars, &HSV.at("Vh"), 255);
+    createTrackbar("Vl", hsvtrackbars, &HSV.at("Vl"), 255);
 
-    createTrackbar("Threshold:", "Contours", &thresh.at("Thresh"), 255);
+    createTrackbar("Threshold:", rgbwindow, &thresh.at("Thresh"), 255);
 
     device.startVideo();
     device.startDepth();
@@ -67,8 +68,12 @@ int main(/*int argc, char *argv[]*/)
     while (!die) {
         device.getVideo(rgbMat);
         device.getDepth(depthMat);
-        imshow(rgbwindow, findObject(rgbMat));
-        imshow("Contours", buildContours(rgbMat));
+        imshow(rgbwindow,
+               differentiateObjects(
+                   rgbMat,
+                   Scalar(HSV.at("Hl"), HSV.at("Sl"), HSV.at("Vl")),
+                   Scalar(HSV.at("Hh"), HSV.at("Sh"), HSV.at("Vh"))));
+//        imshow("Contours", buildContours(rgbMat));
         depthMat.convertTo(depthf, CV_8UC1, 255.0/2048.0);
         imshow("depth",depthf);
 
@@ -171,4 +176,72 @@ Mat buildContours(Mat &orig){
         }
     }
     return drawing;
+}
+
+// differentiateObjects takes an image, extracts a range of
+// colors (HSV lower and upper bounds), and differentiates
+// the objects of that color from each other.
+Mat differentiateObjects(Mat &orig, Scalar HSVlb, Scalar HSVub){
+    Mat imgHSV;
+    cvtColor(orig, imgHSV, COLOR_BGR2HSV);
+
+    // Morphological opening (remove fg objects)
+    erode(imgHSV, imgHSV, getStructuringElement(MORPH_ELLIPSE, Size(5,5)));
+    dilate(imgHSV, imgHSV, getStructuringElement(MORPH_ELLIPSE, Size(5,5)));
+
+    // Morphological closing (remove fg holes)
+    dilate(imgHSV, imgHSV, getStructuringElement(MORPH_ELLIPSE, Size(5,5)));
+    erode(imgHSV, imgHSV, getStructuringElement(MORPH_ELLIPSE, Size(5,5)));
+
+    // Threshold image
+    Mat colorThresh;
+    inRange(imgHSV, HSVlb, HSVub, colorThresh);
+
+    // Declare contour variables
+    Mat HSV_gray;
+    Mat threshout;
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
+
+    // Blur image
+    blur(colorThresh, HSV_gray, Size(30,30));
+
+    // Detect edges and find contours
+    threshold(HSV_gray, threshout, thresh["Thresh"], 255, THRESH_BINARY);
+    findContours(threshout, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0,0));
+
+
+    // Declare polygon variables
+    vector<vector<Point>> contours_poly(contours.size());
+    vector<Rect> boundRects(contours.size());
+    vector<Point2f> center(contours.size());
+    vector<float> radius(contours.size());
+
+    // Declare moment variables
+    vector<Moments> objMomentsVec(contours.size());
+    vector<Point> objCentroids(contours.size());
+
+    // Approximate contours to polygons and build bounding rectangles
+    for(unsigned int i = 0; i < contours.size(); ++i){
+        approxPolyDP(Mat(contours[i]), contours_poly[i], 3, false);
+        boundRects[i] = boundingRect(Mat(contours_poly[i]));
+        minEnclosingCircle((Mat)contours_poly[i], center[i], radius[i]);
+    }
+
+    // Draw bounding rectangles
+    Mat overlay = orig;
+    for(unsigned int i = 0; i < contours.size(); ++i){
+        Scalar color = Scalar(rng.uniform(0,255), rng.uniform(0,255), rng.uniform(0,255));
+        if(radius[i] > 15){
+            drawContours(overlay, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+//            rectangle(overlay, boundRects[i].tl(), boundRects[i].br(), color, 2, 8, 0);
+            objMomentsVec[i] = moments(HSV_gray(boundRects[i]).clone());
+            if(objMomentsVec[i].m00 > 10000){
+                objCentroids[i] = Point(objMomentsVec[i].m10 / objMomentsVec[i].m00,
+                                        objMomentsVec[i].m01 / objMomentsVec[i].m00) + boundRects[i].tl();
+                circle(overlay, objCentroids[i], 2, Scalar(0,255,0),-1);
+            }
+        }
+    }
+    return overlay;
 }
